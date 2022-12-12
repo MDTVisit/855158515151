024c17b1344359d0c7416e4c79aa71231b94fd83
MDTVisit <112145096+MDTVisit@users.noreply.github.com>
2022-12-12 23:40:29 +0700

    asdadadssad

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Tree<string> tree = new Tree<string>();
+
+
+        while(true){
+            string name = Console.ReadLine();
+            int node = int.Parse(Console.ReadLine());
+
+            
+        Console.Write(name);
+
+
+       }
+    }
+}
